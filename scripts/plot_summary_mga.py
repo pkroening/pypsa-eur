bf9074a35416e8a2e77c5915b4b2c729ff32026e
Peter Kröning <peter.kroening@rwth-aachen.de>
2026-07-07 11:19:32 +0200

    Fix plot summary mga header, merge with non mga, remove unused imports

--- scripts/plot_summary_mga.py
+++ scripts/plot_summary_mga.py
@@ -1,5 +1,4 @@
-# -*- coding: utf-8 -*-
-# SPDX-FileCopyrightText: : 2020-2024 The PyPSA-Eur Authors
+# SPDX-FileCopyrightText: Contributors to PyPSA-Eur <https://github.com/pypsa/pypsa-eur> and <https://github.com/koen-vg/eu-hydrogen>, edited by Peter Kröning
 #
 # SPDX-License-Identifier: MIT
 """
@@ -8,35 +7,43 @@ Creates plots from summary CSV files.
 
 import logging
 
-import matplotlib.gridspec as gridspec
 import matplotlib.pyplot as plt
-import pandas as pd
-from _helpers import configure_logging, set_scenario_config
-from plot_summary import (
+
+from scripts._helpers import configure_logging, set_scenario_config
+from scripts.plot_summary import (
     plot_balances,
     plot_carbon_budget_distribution,
     plot_costs,
     plot_energy,
 )
-from prepare_sector_network import co2_emissions_year
 
 logger = logging.getLogger(__name__)
-plt.style.use("ggplot")
+plt.style.use("bmh")
 
 
 if __name__ == "__main__":
     if "snakemake" not in globals():
-        from _helpers import mock_snakemake
+        from scripts._helpers import mock_snakemake
 
-        snakemake = mock_snakemake("plot_summary")
+        snakemake = mock_snakemake(
+            "plot_summary_mga",
+            configfiles="config/test/config.myopic-mga.yaml",
+        )
 
     configure_logging(snakemake)
     set_scenario_config(snakemake)
 
-    n_header = 5
+    n_header = 6
 
     plot_costs(snakemake, n_header)
 
     plot_energy(snakemake, n_header)
 
     plot_balances(snakemake, n_header)
+
+    co2_budget = snakemake.params["co2_budget"]
+    if (
+        isinstance(co2_budget, str) and co2_budget.startswith("cb")
+    ) or snakemake.params["foresight"] == "perfect":
+        options = snakemake.params.sector
+        plot_carbon_budget_distribution(snakemake.input.eurostat, options)
